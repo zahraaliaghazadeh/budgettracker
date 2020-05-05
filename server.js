075c6798ab3d004881ed4bef09cadc0018a20424
Yalda Ali Aghazadeh <zahraaliaghazadeh@Yaldas-MacBook-Pro.local>
2020-05-04 21:29:48 -0700

    wip

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 8085;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+// connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URL ||
+  "mongodb://user1:password1@ds133378.mlab.com:33378/heroku_5n7pz53x", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
